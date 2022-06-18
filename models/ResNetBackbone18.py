9a133c5bccb46edf9f042faa21d3085e4f38089f
Cristian <tinaburricristian@libero.it>
2022-06-18 13:25:47 +0200

    Update ResNetBackbone18.py

--- models/ResNetBackbone18.py
+++ models/ResNetBackbone18.py
@@ -12,36 +12,40 @@ class ResNetBackbone18(nn.Module):
 
     resnet.conv1.weight = nn.Parameter(resnet.conv1.weight.sum(dim=1).unsqueeze(1)) 
 
-    self.midlevel_resnet = nn.Sequential(*list(resnet.children())[0:6])
+    self.resnet_backbone = nn.Sequential(*list(resnet.children())[0:6])
 
     for param in self.midlevel_resnet.parameters():
       param.requires_grad = False
 
-    self.upsample = nn.Sequential(     
-      nn.Conv2d(128, 32, kernel_size=3, stride=1, padding=1),
-      nn.BatchNorm2d(32),
-      nn.ReLU(),
-      nn.Upsample(scale_factor=2),
-      nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
-      nn.BatchNorm2d(32),
-      nn.ReLU(),
-      nn.Dropout(self.percentage_dropout),
-      nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+    self.custom_layer = nn.Sequential(     
+      nn.Conv2d(128, 64, kernel_size=3, stride=1, padding=1),
       nn.BatchNorm2d(64),
       nn.ReLU(),
-      nn.Upsample(scale_factor=2),
+      nn.ConvTranspose2d(64, 64, kernel_size=2, stride=2),
+
+      nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+      nn.BatchNorm2d(64),
+      nn.ReLU(),
+      nn.Dropout(0.5),
+
       nn.Conv2d(64, 32, kernel_size=3, stride=1, padding=1),
       nn.BatchNorm2d(32),
       nn.ReLU(),
-      nn.Dropout(self.percentage_dropout),
-      nn.Conv2d(32, 2, kernel_size=3, stride=1, padding=1),
-      nn.Upsample(scale_factor=2)
+      nn.ConvTranspose2d(32, 64, kernel_size=2, stride=2),
+
+      nn.Conv2d(64, 32, kernel_size=3, stride=1, padding=1),
+      nn.BatchNorm2d(32),
+      nn.ReLU(),
+      nn.Dropout(0.5),
+      
+      nn.Conv2d(32, 2, kernel_size=3, stride=1, padding=0),
+      nn.ConvTranspose2d(2, 2, kernel_size=2, stride=2),
     )
 
   def forward(self, input):
 
-    midlevel_features = self.midlevel_resnet(input)
+    resnet_backbone = self.resnet_backbone(input)
 
-    output = self.upsample(midlevel_features)
+    output = self.custom_layer(midlevel_features)
     
     return output
